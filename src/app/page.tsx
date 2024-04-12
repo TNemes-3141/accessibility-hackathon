f6f56ed35c6c2f81713299c1aebeda2f6c95c075
Thomas Jaggi <thomas@responsive.ch>
2024-04-12 11:48:22 +0200

    feat: move button description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,11 +20,11 @@ export default function Home() {
             <button
               type="button"
               className="absolute bottom-0 border-radius-50"
-              aria-label="Fragen zum Bild stellen"
               onClick={() => {
                 dialogRef.current?.showModal();
               }}
             >
+              <span className="sr-only">Fragen zum Bild stellen</span>
               {/* eslint-disable-next-line @next/next/no-img-element */}
               <svg
                 style={{ stroke: "white", fill: "white" }}
@@ -33,6 +33,7 @@ export default function Home() {
                 viewBox="0 0 24 24"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
+                role="presentation"
               >
                 <path
                   d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22Z"
