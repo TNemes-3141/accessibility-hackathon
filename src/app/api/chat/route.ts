736bf0dcdd23b327b10c9e23c9eba840e26189b8
Thomas Jaggi <thomas@responsive.ch>
2024-04-12 15:04:26 +0200

    feat: submit message history

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -4,27 +4,33 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
-export type ChatResponse = {
+export type ChatMessage = {
+  message: string;
+  role: "user" | "system";
+};
+
+export type ChatInput =
+  | {
+      message: ChatMessage["message"];
+      history?: Array<ChatMessage>;
+    }
+  | {
+      init: {
+        productImage: string;
+        productImageAlt?: string;
+        productDescription: string;
+      };
+    };
+
+export type ChatOutput = {
   message?: string | null;
   action?: "close" | "buy";
-};
-
-export type ChatInput = {
-  message: string;
-};
-
-export type ChatInputInit = {
-  init: {
-    productImage: string;
-    productImageAlt?: string;
-    productDescription: string;
-  };
 };
 
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { message, init } = await req.json();
+  const { message, history, init } = await req.json();
 
   if (init) {
     // TODO: fetch initial response
@@ -42,7 +48,7 @@ export async function POST(req: Request) {
       },
     ],
   });
-  const response: ChatResponse = {
+  const response: ChatOutput = {
     message: apiResponse.choices[0].message.content,
   };
 
