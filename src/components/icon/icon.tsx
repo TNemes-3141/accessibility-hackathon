2fe31439b099b74f5604e3aa36066be132e0e99e
Thomas Jaggi <thomas@responsive.ch>
2024-04-12 19:17:29 +0200

    feat: update rating

--- src/components/icon/icon.tsx
+++ src/components/icon/icon.tsx
@@ -104,3 +104,23 @@ export function Submit() {
     </svg>
   );
 }
+
+export function Star() {
+  return (
+    <svg
+      width="18"
+      height="18"
+      viewBox="0 0 36 34"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      role="presentation"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M9.00003 21.5605L6.87542 33.5L18 27.8629L29.1246 33.5L27 21.5605L36 13.1049L23.5623 11.3629L18 0.5L12.4377 11.3629L3.05176e-05 13.1049L9.00003 21.5605Z"
+        fill="black"
+      />
+    </svg>
+  );
+}
--- src/components/ratingStars/ratingStars.tsx
+++ src/components/ratingStars/ratingStars.tsx
@@ -1,81 +1,12 @@
+import { Star } from "../icon/icon";
+
 export const RatingStars = () => (
   <div className="flex">
-    <span className="sr-only">4 von 5 Sternen</span>
-    <svg
-      width="18"
-      height="18"
-      viewBox="0 0 36 34"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      role="presentation"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M9.00003 21.5605L6.87542 33.5L18 27.8629L29.1246 33.5L27 21.5605L36 13.1049L23.5623 11.3629L18 0.5L12.4377 11.3629L3.05176e-05 13.1049L9.00003 21.5605Z"
-        fill="black"
-      />
-    </svg>
-
-    <svg
-      width="18"
-      height="18"
-      viewBox="0 0 36 34"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      role="presentation"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M9.00003 21.5605L6.87542 33.5L18 27.8629L29.1246 33.5L27 21.5605L36 13.1049L23.5623 11.3629L18 0.5L12.4377 11.3629L3.05176e-05 13.1049L9.00003 21.5605Z"
-        fill="black"
-      />
-    </svg>
-    <svg
-      width="18"
-      height="18"
-      viewBox="0 0 36 34"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      role="presentation"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M9.00003 21.5605L6.87542 33.5L18 27.8629L29.1246 33.5L27 21.5605L36 13.1049L23.5623 11.3629L18 0.5L12.4377 11.3629L3.05176e-05 13.1049L9.00003 21.5605Z"
-        fill="black"
-      />
-    </svg>
-    <svg
-      width="18"
-      height="18"
-      viewBox="0 0 36 34"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      role="presentation"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M9.00003 21.5605L6.87542 33.5L18 27.8629L29.1246 33.5L27 21.5605L36 13.1049L23.5623 11.3629L18 0.5L12.4377 11.3629L3.05176e-05 13.1049L9.00003 21.5605Z"
-        fill="black"
-      />
-    </svg>
-    <svg
-      width="18"
-      height="18"
-      viewBox="0 0 36 34"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      role="presentation"
-    >
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M9.00003 21.5605L6.87542 33.5L18 27.8629L29.1246 33.5L27 21.5605L36 13.1049L23.5623 11.3629L18 0.5L12.4377 11.3629L3.05176e-05 13.1049L9.00003 21.5605Z"
-        fill="black"
-      />
-    </svg>
+    <span className="sr-only">5 von 5 Sternen</span>
+    <Star />
+    <Star />
+    <Star />
+    <Star />
+    <Star />
   </div>
 );
